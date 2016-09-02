23469e4bc8a68d9c002051680b3f7de90fc81d4d
Tadeu Araujo <tadeu.araujo@somoseducacao.com.br>
2016-09-02 17:17:14 -0300

    Customização para garantir que itens zerados não apareçam no chart.

--- Charts/Classes/Renderers/PieChartRenderer.swift
+++ Charts/Classes/Renderers/PieChartRenderer.swift
@@ -333,6 +333,12 @@ public class PieChartRenderer: ChartDataRendererBase
                 
                 guard let e = dataSet.entryForIndex(j) else { continue }
                 
+                // draw only if the value is greater than zero
+                if ((abs(e.value) <= 0.000001)) {
+                    xIndex += 1
+                    continue
+                }
+                
                 if (xIndex == 0)
                 {
                     angle = 0.0
